e391814ff026fadedcffc070acf6f48e22d5e638
HeinThantAung <heinthant098@gmail.com>
2023-12-04 16:48:54 +0630

    feat:task1とtask1のテストケースの実装

---
+++ src/main/java/com/validationtask/task1/UserRequest.java
@@ -0,0 +1,58 @@
+package com.validationtask.task1;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class UserRequest {
+
+    @NotNull(message = "ユーザー名を入力してください")
+    @Size(min = 3, max = 20, message = "ユーザー名は3文字以上20文字以下である必要があります")
+    private String username;
+
+
+    @NotNull(message = "パスワードを入力してください")
+    @Size(min = 8, max = 30, message = "パスワードは8文字以上30文字以下である必要があります")
+    private String password;
+
+    private String confirmPassword;
+
+    public UserRequest(String username, String password, String confirmPassword) {
+        this.username = username;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    @AssertTrue(message = "パスワードと確認用パスワードが一致していません")
+    public boolean isPasswordMatching() {
+        if (password == null || password.isEmpty() || password.equals(confirmPassword)) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ src/test/java/com/validationtask/task1/UserRequestTest.java
@@ -0,0 +1,94 @@
+package com.validationtask.task1;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+
+public class UserRequestTest {
+    private static Validator validator;
+
+    @BeforeAll
+    public static void setUpValidator() {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+
+    @Test
+    public void usernameとpasswordとconfirmPasswordがnullの時にバリデーションエラーとなること() {
+        UserRequest userRequest = new UserRequest(null, null, null);
+        Set<ConstraintViolation<UserRequest>> violations = validator.validate(userRequest);
+        assertThat(violations).hasSize(2);
+        assertThat(violations)
+                .extracting(violation -> violation.getPropertyPath().toString(), ConstraintViolation::getMessage)
+                .containsExactlyInAnyOrder(tuple("username", "ユーザー名を入力してください"), tuple("password", "パスワードを入力してください"));
+    }
+
+    @Test
+    public void usernameとpasswordとconfirmPasswordが空文字の時にバリデーションエラーとなること() {
+        UserRequest userRequest = new UserRequest("", "", "");
+        Set<ConstraintViolation<UserRequest>> violations = validator.validate(userRequest);
+        assertThat(violations).hasSize(2);
+        assertThat(violations)
+                .extracting(violation -> violation.getPropertyPath().toString(), ConstraintViolation::getMessage)
+                .containsExactlyInAnyOrder(tuple("username", "ユーザー名は3文字以上20文字以下である必要があります"), tuple("password", "パスワードは8文字以上30文字以下である必要があります"));
+    }
+
+    @Test
+    public void usernameが3文字以下入力された時にバリテーションエラーとなること() {
+        UserRequest userRequest = new UserRequest("us", "password", "password");
+        Set<ConstraintViolation<UserRequest>> violations = validator.validate(userRequest);
+        assertThat(violations).hasSize(1);
+        assertThat(violations)
+                .extracting(violation -> violation.getPropertyPath().toString(), ConstraintViolation::getMessage)
+                .containsExactlyInAnyOrder(tuple("username", "ユーザー名は3文字以上20文字以下である必要があります"));
+    }
+
+    @Test
+    public void usernameが20文字以上入力された時にバリテーションエラーとなること() {
+        UserRequest userRequest = new UserRequest("useruseruseruseruseruser", "password", "password");
+        Set<ConstraintViolation<UserRequest>> violations = validator.validate(userRequest);
+        assertThat(violations).hasSize(1);
+        assertThat(violations)
+                .extracting(violation -> violation.getPropertyPath().toString(), ConstraintViolation::getMessage)
+                .containsExactlyInAnyOrder(tuple("username", "ユーザー名は3文字以上20文字以下である必要があります"));
+    }
+
+    @Test
+    public void passwordが8文字以下入力された時にバリテーションエラーとなること() {
+        UserRequest userRequest = new UserRequest("user", "pass", "pass");
+        Set<ConstraintViolation<UserRequest>> violations = validator.validate(userRequest);
+        assertThat(violations).hasSize(1);
+        assertThat(violations)
+                .extracting(violation -> violation.getPropertyPath().toString(), ConstraintViolation::getMessage)
+                .containsExactlyInAnyOrder(tuple("password", "パスワードは8文字以上30文字以下である必要があります"));
+    }
+
+    @Test
+    public void passwordが30文字以上入力された時にバリテーションエラーとなること() {
+        UserRequest userRequest = new UserRequest("user", "passwordpasswordpasswordpassword", "passwordpasswordpasswordpassword");
+        Set<ConstraintViolation<UserRequest>> violations = validator.validate(userRequest);
+        assertThat(violations).hasSize(1);
+        assertThat(violations)
+                .extracting(violation -> violation.getPropertyPath().toString(), ConstraintViolation::getMessage)
+                .containsExactlyInAnyOrder(tuple("password", "パスワードは8文字以上30文字以下である必要があります"));
+    }
+
+    @Test
+    public void passwordとconfirmPasswordが一致していない時にバリテーションエラーとなること() {
+        UserRequest userRequest = new UserRequest("user", "password", "passwdro");
+        Set<ConstraintViolation<UserRequest>> violations = validator.validate(userRequest);
+        assertThat(violations).hasSize(1);
+        assertThat(violations)
+                .extracting(violation -> violation.getPropertyPath().toString(), ConstraintViolation::getMessage)
+                .containsExactlyInAnyOrder(tuple("passwordMatching", "パスワードと確認用パスワードが一致していません"));
+    }
+}
